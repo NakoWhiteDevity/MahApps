c9e599c8f4247089c304cd834cac6b92a9c8bade
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-03-29 12:55:56 +0200

    El commit de antes de comer. Descubrimos que podemos añadir un ! posterior a una variable que nos indica que no esta inicializada en el constructor para que no se nos queje, y decirle que los datos llegaran '@input() jugadahija!:Chinaface;'

--- buscador/src/app/componentes/main/main.component.ts
+++ buscador/src/app/componentes/main/main.component.ts
@@ -14,38 +14,6 @@ export class MainComponent implements OnInit {
     this._jh.getJSON().subscribe( (resp:Chinaface[]) => this.jugadas = resp );
   }
 
-  //Funciones transformadoras:
-  tiposprint(arraydetipos:number[]):string[]{
-    let arraytraducido:string[] = [];
-    arraydetipos.forEach(tipo => {
-      switch(tipo){
-        case 1: arraytraducido.push("chi") ; break ;
-        case 2: arraytraducido.push("pong") ; break ;
-        case 3: arraytraducido.push("kong") ; break ;
-        case 4: arraytraducido.push("ponkon") ; break ;
-        case 5: arraytraducido.push("accion") ; break ;
-        case 6: arraytraducido.push("filtro") ; break ;
-        case 7: arraytraducido.push("manoculta") ; break ;
-        case 8: arraytraducido.push("familias") ; break ;
-        case 9: arraytraducido.push("simples") ; break ;
-        case 10: arraytraducido.push("terminales") ; break ;
-        case 11: arraytraducido.push("honores") ; break ;
-        case 12: arraytraducido.push("vientos") ; break ;
-        case 13: arraytraducido.push("dragones") ; break ;
-      }
-    });
-    return arraytraducido;
-  }
-
-  packsprint(packs:number,pareja:boolean):string{
-   const parejastring = (pareja:boolean) => { if (pareja == true){ return "P" } else { return "" } };
-   const packisnull = (packs:number) => { if (packs == 0){ return "" } else { return packs } } ;
-   let arraydevolver:string = "";
-   arraydevolver = arraydevolver.concat(`${packisnull(packs)}`);
-   arraydevolver = arraydevolver.concat(`${parejastring(pareja)}`);
-   return arraydevolver;
-  };
-
   ngOnInit(): void {}
 
 }
