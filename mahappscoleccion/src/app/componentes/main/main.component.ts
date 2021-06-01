23fd097ec1c56cfa6a56cb46863c3566301320e9
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-06-01 18:07:21 +0200

    checkpoint de hoy por hoy

--- mahappscoleccion/src/app/componentes/main/main.component.ts
+++ mahappscoleccion/src/app/componentes/main/main.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  conmutador = { buscador : false, quiz : false, letrero : false }
+  objeto:boolean = true;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  accionconmutadora(enciende:string){
+    switch(enciende){
+      case 'buscador' : {this.conmutador.buscador = true ; this.conmutador.quiz = false ; this.conmutador.letrero = false} ; break ;
+      case 'quiz' : {this.conmutador.buscador = false ; this.conmutador.quiz = true ; this.conmutador.letrero = false} ; break ;
+      case 'letrero' : {this.conmutador.buscador = false ; this.conmutador.quiz = false ; this.conmutador.letrero = true} ; break ;
+    }
   }
 
 }
