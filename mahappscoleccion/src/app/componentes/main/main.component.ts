b50a57fef4247bb6a9c601891d0cd6c2a7e45d70
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-06-05 12:20:16 +0200

    Aprendemos una forma bien curiosa de deshabilitar botones a traves de css

--- mahappscoleccion/src/app/componentes/main/main.component.ts
+++ mahappscoleccion/src/app/componentes/main/main.component.ts
@@ -7,8 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  funcionamiento = {
+    conmutadores : {
+      buscador : false,
+      quiz : false,
+      letrero : false
+    },
+    opciones : {
+      buscador : [
+        ["variante china",""],
+        ["variante hongkonesa","https://www.google.es"],
+        ["variante japonesa","https://www.google.es"]
+      ],
+      quiz : {
+        opcion1: {
+          nombre : "variante china",
+          enlace : "el que fuera"
+        },
+        opcion2: {
+          nombre : "variante hongkonesa",
+          enlace : null
+        },
+        opcion3: {
+          nombre : "variante japonesa",
+          enlace : null
+        }
+      },
+      letrero : {
+        opcion1 : {
+          nombre : "variante china",
+          enlace : "El que fuera"
+        } ,
+        opcion2 : {
+          nombre : "variante hongkonesa",
+          enlace : "El que fuera"
+        } ,
+        opcion3 : {
+          nombre : "variante japonesa",
+          enlace : null
+        }
+      }
+    }
+  }
   conmutador = { buscador : false, quiz : false, letrero : false }
   objeto:boolean = true;
+  
   
   constructor() { }
 
@@ -17,9 +60,9 @@ export class MainComponent implements OnInit {
 
   accionconmutadora(enciende:string){
     switch(enciende){
-      case 'buscador' : {this.conmutador.buscador = true ; this.conmutador.quiz = false ; this.conmutador.letrero = false} ; break ;
-      case 'quiz' : {this.conmutador.buscador = false ; this.conmutador.quiz = true ; this.conmutador.letrero = false} ; break ;
-      case 'letrero' : {this.conmutador.buscador = false ; this.conmutador.quiz = false ; this.conmutador.letrero = true} ; break ;
+      case 'buscador' : {this.funcionamiento.conmutadores.buscador = true ; this.funcionamiento.conmutadores.quiz = false ; this.funcionamiento.conmutadores.letrero = false} ; break ;
+      case 'quiz' : {this.funcionamiento.conmutadores.buscador = false ; this.funcionamiento.conmutadores.quiz = true ; this.funcionamiento.conmutadores.letrero = false} ; break ;
+      case 'letrero' : {this.funcionamiento.conmutadores.buscador = false ; this.funcionamiento.conmutadores.quiz = false ; this.funcionamiento.conmutadores.letrero = true} ; break ;
     }
   }
 
