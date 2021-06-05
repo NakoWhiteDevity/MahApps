1d1488d7e2abff6fd688c024168f1f8eff98ab6c
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-06-05 12:36:03 +0200

    Vamos a empezar a implantar las aplicaciones

--- mahappscoleccion/src/app/componentes/main/main.component.ts
+++ mahappscoleccion/src/app/componentes/main/main.component.ts
@@ -16,42 +16,21 @@ export class MainComponent implements OnInit {
     opciones : {
       buscador : [
         ["variante china",""],
-        ["variante hongkonesa","https://www.google.es"],
-        ["variante japonesa","https://www.google.es"]
+        ["variante hongkonesa",""],
+        ["variante japonesa",""]
       ],
-      quiz : {
-        opcion1: {
-          nombre : "variante china",
-          enlace : "el que fuera"
-        },
-        opcion2: {
-          nombre : "variante hongkonesa",
-          enlace : null
-        },
-        opcion3: {
-          nombre : "variante japonesa",
-          enlace : null
-        }
-      },
-      letrero : {
-        opcion1 : {
-          nombre : "variante china",
-          enlace : "El que fuera"
-        } ,
-        opcion2 : {
-          nombre : "variante hongkonesa",
-          enlace : "El que fuera"
-        } ,
-        opcion3 : {
-          nombre : "variante japonesa",
-          enlace : null
-        }
-      }
+      quiz : [
+        ["variante china",""],
+        ["variante hongkonesa",""],
+        ["variante japonesa",""]
+      ],
+      letrero : [
+        ["variante china",""],
+        ["variante hongkonesa",""],
+        ["variante japonesa",""]
+      ]
     }
   }
-  conmutador = { buscador : false, quiz : false, letrero : false }
-  objeto:boolean = true;
-  
   
   constructor() { }
 
